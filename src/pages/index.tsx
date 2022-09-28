b7d95001f3d0c0ef6656fd3fe881c9b7c2d39ab5
Spencer Adolph <spenceradolph@ymail.com>
2022-09-27 22:48:04 -0500

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Scanning Site!</a>
+          Welcome to <a>Scanning Site!</a>
         </h1>
 
         <p className={styles.description}>Get started now!</p>
